180a27027356127a48447f518e61f5ca39a758bd
joaolessab <joaovitorlessa@gmail.com>
2023-10-29 12:35:42 -0300

    update(app): basic app view layout

--- goals-app/App.js
+++ goals-app/App.js
@@ -1,17 +1,49 @@
+import { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [goals, setGoals] = useState([]);
+
+  const goalInputHandler = enteredText => {
+    setEnteredGoalText(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    // best approach to set new value depending on a previous status, 
+    // it's using an arrow function
+    setGoals(currentGoals => [
+      ...currentGoals,
+      enteredGoalText
+    ]);
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.textInput}
           placeholder='Your course goal!'
+          onChangeText={goalInputHandler}
         />
-        <Button title="Add Goal" />
+        <Button
+          title="Add Goal"
+          onPress={addGoalHandler}
+        />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
+        {goals.map((goal) =>
+          <View 
+            key={goal}
+            style={styles.goalItem}
+          >
+            <Text
+              style={styles.goalText}
+            >
+              {goal}
+            </Text>
+          </View>
+        )}
       </View>
     </View>
   );
@@ -41,5 +73,14 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5E0ACC',
+  },
+  goalText: {
+    color: 'white',
   }
  });
